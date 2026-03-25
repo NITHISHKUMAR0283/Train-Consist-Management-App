5bdf99c62a35196d746d00359653080944aaef95
NithishKumar A <nk0283@srmist.edu.in>
2026-03-25 09:42:04 +0530

    initial commit

--- src/TrainConsistApp.java
+++ src/TrainConsistApp.java
@@ -1,18 +1,19 @@
-import java.util.LinkedHashSet;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TrainConsistApp {
     public static void main(String[] args) {
         System.out.println("=== Train Consist Management App ===");
 
-        LinkedHashSet<String> trainFormation = new LinkedHashSet<>();
+        HashMap<String, Integer> bogieCapacity = new HashMap<>();
 
-        trainFormation.add("Engine");
-        trainFormation.add("Sleeper");
-        trainFormation.add("Cargo");
-        trainFormation.add("Guard");
+        bogieCapacity.put("Sleeper", 72);
+        bogieCapacity.put("AC Chair", 60);
+        bogieCapacity.put("First Class", 48);
 
-        trainFormation.add("Sleeper"); // Attempt duplicate
-
-        System.out.println("Final train formation (in insertion order): " + trainFormation);
+        System.out.println("Bogie capacities:");
+        for (Map.Entry<String, Integer> entry : bogieCapacity.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue() + " seats");
+        }
     }
 }
