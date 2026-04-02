a6959aa72d252f108851d91c1fe8d734e5777734
Nithish Kumar <nk0283@srmist.edu.in>
2026-04-02 09:25:18 +0530

    Initial commit at feature/uc10

--- src/TrainConsistApp.java
+++ src/TrainConsistApp.java
@@ -20,18 +20,11 @@ public class TrainConsistApp {
         bogies.add(new Bogie("Sleeper", 72));
         bogies.add(new Bogie("AC Chair", 60));
         bogies.add(new Bogie("First Class", 48));
-        bogies.add(new Bogie("Sleeper", 70));
-        bogies.add(new Bogie("AC Chair", 55));
 
-        Map<String, List<Bogie>> groupedBogies = bogies.stream()
-                .collect(Collectors.groupingBy(b -> b.name));
+        int totalSeats = bogies.stream()
+                .map(b -> b.capacity)
+                .reduce(0, Integer::sum);
 
-        System.out.println("Grouped bogies by type:");
-        for (Map.Entry<String, List<Bogie>> entry : groupedBogies.entrySet()) {
-            System.out.println(entry.getKey() + ":");
-            for (Bogie b : entry.getValue()) {
-                System.out.println("  " + b.capacity + " seats");
-            }
-        }
+        System.out.println("Total seating capacity: " + totalSeats);
     }
 }
